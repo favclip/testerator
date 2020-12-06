5a3124095755c28bb91c199f628944eed07447da
vvakame <vvakame+dev@gmail.com>
2020-12-06 20:20:37 +0900

    emit message when use Disable1stGen = true with datastore, memcache, search package

--- memcache/memcache.go
+++ memcache/memcache.go
@@ -2,6 +2,8 @@ package memcache
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"github.com/favclip/testerator"
 	"google.golang.org/appengine/memcache"
@@ -9,6 +11,9 @@ import (
 
 func init() {
 	testerator.DefaultSetup.Cleaners = append(testerator.DefaultSetup.Cleaners, func(s *testerator.Setup) error {
+		if s.Disable1stGen {
+			_, _ = fmt.Fprintln(os.Stderr, `don't use "github.com/favclip/testerator/memcache" package with Disable1stGen`)
+		}
 		return Cleanup(s.Context)
 	})
 }
